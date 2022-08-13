2e007bc6912f2e66f2e3c877d944de7e5fb31f00
ihavezerohealth <michallahcim01@gmail.com>
2022-08-13 19:14:39 +0100

    Everything works, ish. Sensitivity is still [redacted]-level....

--- lib/instruments/ga_radio.dart
+++ lib/instruments/ga_radio.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:segment_display/segment_display.dart';
-import 'dart:async';
 import 'dart:core';
 
 class GARadioView extends StatefulWidget {
@@ -18,11 +17,54 @@ class _GARadioViewState extends State<GARadioView> {
   num com2Standby = 124.457;
 
   double distance = 0;
-  final ratio = 14;
+  final ratio = 55;
+
+  num frequencyRound(num value) {
+    num temp = value % 0.025;
+    if (temp < 0) {
+      temp = 0.025 + temp;
+    } else if (temp == 0) {
+      return value;
+    }
+    return value + 0.025 - temp;
+  }
+
+  bool frequencyLimit(num value) {
+    if (value > 117.975 && value < 137.000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void handleFrequenciesCom1(num value) {
+    num rounded = frequencyRound(value);
+    bool limited = frequencyLimit(rounded);
+    print(limited);
+    if (limited) {
+      com1Active = rounded;
+    } else if (com1Active.compareTo(117.975).isNegative) {
+      com1Active = 117.975;
+    } else {
+      com1Active = 137.000;
+    }
+  }
+  void handleFrequenciesCom2(num value) {
+    num rounded = frequencyRound(value);
+    bool limited = frequencyLimit(rounded);
+    print(limited);
+    if (limited) {
+      com2Active = rounded;
+    } else if (com2Active.compareTo(117.975).isNegative) {
+      com2Active = 117.975;
+    } else {
+      com2Active = 137.000;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return Scaffold(
       backgroundColor: Colors.black54,
       body: Column(
@@ -30,16 +72,14 @@ class _GARadioViewState extends State<GARadioView> {
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 5),
-            child: Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text("GA RADIO", style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.white60
-                  ))
-                ],
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("GA RADIO", style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white60
+                ))
+              ],
             ),
           ),
           Expanded(
@@ -62,10 +102,15 @@ class _GARadioViewState extends State<GARadioView> {
                     onHorizontalDragStart: (details) {distance = 0.00;},
                     onHorizontalDragUpdate: (details) {
                       //distance += details.primaryDelta!;
-                      com1Active += num.parse((details.primaryDelta! / ratio).toStringAsFixed(3));
+                      print("ratio $ratio");
+                      print("line2 ${details.primaryDelta! / ratio}");
+                      print("line3 ${(details.primaryDelta! / ratio).toStringAsFixed(3)}");
+                      num temp = com1Active + num.parse((details.primaryDelta! / ratio).toStringAsFixed(3));
+                      print("temp1 $temp");
+                      handleFrequenciesCom1(temp);
                       setState(() {});
                     },
-                    onHorizontalDragEnd: (details) {print(distance);},
+                    //onHorizontalDragEnd: (details) {handleFrequenciesCom1(com1Active); setState(() {});},
                     child: const Image(
                       image: AssetImage("assets/ga_radio/knob.png"),
                       height: 100,
@@ -95,14 +140,16 @@ class _GARadioViewState extends State<GARadioView> {
                       num temp = com2Active;
                       com2Active = com2Standby;
                       com2Standby = temp;
+                      setState(() {});
                     },
                     onHorizontalDragStart: (details) {distance = 0.00;},
                     onHorizontalDragUpdate: (details) {
                       //distance += details.primaryDelta!;
                       com2Active += num.parse((details.primaryDelta! / ratio).toStringAsFixed(3));
+                      handleFrequenciesCom2(com2Active);
                       setState(() {});
                     },
-                    onHorizontalDragEnd: (details) {print(distance);},
+                    onHorizontalDragEnd: (details) {handleFrequenciesCom2(com2Active); setState(() {});},
                     child: const Image(
                         image: AssetImage("assets/ga_radio/knob.png"),
                         height: 100,
@@ -120,5 +167,11 @@ class _GARadioViewState extends State<GARadioView> {
         ],
       )
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   }
 }
